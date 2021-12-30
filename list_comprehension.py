3c6eecc0580f01ef8e8bacfc62524d2bf4b9f118
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2021-12-30 15:49:37 +0100

    LISTS practice

---
+++ list_comprehension.py
@@ -0,0 +1,2 @@
+my_list = [v for v in range(1, 100) if v % 2 != 0]
+print(my_list)
---
+++ lists.py
@@ -0,0 +1,17 @@
+list = [10,20,30,40]
+
+for element in list:
+  print(element)
+print(list)
+
+list.append(5)
+list.append(14)
+list.append(56)
+list.append(31)
+print(list)
+
+list.reverse()
+print(list)
+
+list.sort()
+print(list)
