4acd53e6b35bf5fda89e28d88acf1208ed6c9722
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2021-12-31 19:30:33 +0100

    TUPLES practice

--- if.py
+++ if.py
@@ -1,4 +1,4 @@
-a = 1
+a = int(input('Wybierz 1 lub 0: '))
 if a == 0:
   print('Zgadza się!')
 else:
---
+++ tuples.py
@@ -0,0 +1,8 @@
+values = (1,2,3,'abc',True)
+
+print(values)
+
+for element in values:
+  print(element)
+
+print(values[1])
