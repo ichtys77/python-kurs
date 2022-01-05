940379fc656e6a55277b4fc1c2eae5c99f06a4c5
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2022-01-05 09:33:52 +0100

    MODULES practice

---
+++ fundaments/own_module.py
@@ -0,0 +1,4 @@
+x = 1
+
+def my_func(name):
+    return 'Witaj! ' + name
---
+++ fundaments/program.py
@@ -0,0 +1,6 @@
+# import own_module
+from own_module import my_func
+
+msg = my_func('Ksawery')
+print(msg)
+# print(own_module.x)
