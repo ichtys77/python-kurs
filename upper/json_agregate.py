c4af91f33e3ca0c116790c4e11f668367820dd7c
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2022-01-16 15:44:48 +0100

    Selective moving files

--- upper/json_agregate.py
+++ upper/json_agregate.py
@@ -2,13 +2,15 @@ import glob
 import os
 import shutil
 import json
+import re
 
 try:
   os.mkdir('./processed')
 except OSError:
   print("'/processed' directory already exist")
 
-receipts = glob.glob('./receipts/receipt-[0-9]*.json')
+# receipts = glob.glob('./receipts/receipt-[0-9]*.json')
+receipts = [f for f in glob.glob('./receipts/receipt-[0-9]*.json') if re.match(r'./receipts\\receipt-[0-9]*[02468].json', f)]
 print(receipts)
 subtotal = 0.0
 
@@ -16,8 +18,9 @@ for path in receipts:
   with open(path) as f:
     content = json.load(f)
     subtotal += float(content['value'])
-  name = path.split('\\')[-1]
-  destination = f'./processed\{name}'
+  # name = path.split('\\')[-1]
+  # destination = f'./processed\{name}'
+  destination = path.replace('receipts', 'processed')
   shutil.move(path, destination)
   print(f"moved '{path}' to '{destination}'")
 
